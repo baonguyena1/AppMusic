a153e95565d592ffdb5f4af740dee6110498d20a
Bao Nguyen <baonguyena1@gmail.com>
2016-10-15 22:26:27 +0700

    Handle play audio with slider

--- AppMusic/AppMusic/ViewController.swift
+++ AppMusic/AppMusic/ViewController.swift
@@ -12,22 +12,60 @@ import AVFoundation
 class ViewController: UIViewController {
     
     var avAudioPlayer: AVAudioPlayer!
+    var avPlayer: AVPlayer!
+    let musicArray = ["Anywhere I Go", "Atinge"]
+    var timer: Timer?
+    
+    @IBOutlet weak var durationSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let path = Bundle.main.path(forResource: "Anywhere I Go", ofType: "mp3") else {
+        playMusicAt(0)
+        playMusicOnline()
+    }
+    
+    func playMusicAt(_ index:Int) {
+        guard let path = Bundle.main.path(forResource: musicArray[index], ofType: "mp3") else {
             return
         }
         let url: URL = URL(fileURLWithPath: path)
         do {
             avAudioPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: "mp3")
+            durationSlider.minimumValue = 0.0
+            durationSlider.maximumValue = Float(avAudioPlayer.duration)
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(changeDurationFollowPlayer), userInfo: nil, repeats: true)
+            avAudioPlayer.delegate = self
             avAudioPlayer.play()
         } catch let error {
             print(error)
         }
     }
     
+    func playMusicOnline() {
+        let urlString = ""
+        if let url = URL(string: urlString) {
+            avPlayer = AVPlayer(url: url);
+            avPlayer.play()
+        }
+    }
     
+    @IBAction func changeDurationTime(_ sender: AnyObject) {
+        avAudioPlayer.pause()
+        avAudioPlayer.currentTime = TimeInterval(durationSlider.value)
+        avAudioPlayer.play()
+    }
+    
+    func changeDurationFollowPlayer() {
+        durationSlider.value = Float(avAudioPlayer.currentTime)
+    }
+}
+
+extension ViewController: AVAudioPlayerDelegate {
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if flag {
+            playMusicAt(1)
+        }
+    }
 }
 
